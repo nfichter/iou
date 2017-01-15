9fa0cbf65180b80e8ab811ca577e033258fd7240
nfichter <nfichter@stuy.edu>
2017-01-15 15:11:10 -0500

    login, register

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def landing():
         return redirect("/home")
     return render_template("landing.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/home")
 def home():
     if not "username" in session:
---
+++ utils/init.py
@@ -0,0 +1,20 @@
+import sqlite3
+import os
+
+keyFile = open("data/key", "w")
+keyFile.write(os.urandom(32))
+keyFile.close()
+
+f = "data/database.db"
+
+db = sqlite3.connect(f)
+c = db.cursor()
+
+q = "CREATE TABLE users(username TEXT, password TEXT, userID INTEGER)"
+c.execute(q)
+
+q = "CREATE TABLE ious(note TEXT, amount INTEGER, dateCreated FLOAT, dateModified FLOAT, userIDLender INTEGER, userIDBorrower INTEGER, completed INTEGER)"
+c.execute(q)
+
+db.commit()
+db.close()
